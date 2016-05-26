7139b715984f6f4e46d21ed18cb50b4de15d050f
Jawnnypoo <jawnnypoo@gmail.com>
2016-05-26 13:13:23 -0500

    Show some loading when the stuff is loading

--- app/src/main/java/com/commit451/commitspiration/activity/MainActivity.kt
+++ app/src/main/java/com/commit451/commitspiration/activity/MainActivity.kt
@@ -7,6 +7,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
+import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import com.commit451.commitspiration.R
@@ -23,6 +24,7 @@ class MainActivity : AppCompatActivity() {
     var mRoot: ViewGroup? = null
     var mToolbar: Toolbar? = null
     var mMessage: TextView? = null
+    var mProgress: View? = null;
 
     var mWhatTheCommitData: WhatTheCommitData? = null
 
@@ -33,6 +35,7 @@ class MainActivity : AppCompatActivity() {
         mToolbar = findViewById(R.id.toolbar) as Toolbar?
         mRoot = findViewById(R.id.root) as ViewGroup?
         mMessage = findViewById(R.id.message) as TextView?
+        mProgress = findViewById(R.id.progress);
 
         mToolbar?.setTitle(R.string.app_name)
         mToolbar?.inflateMenu(R.menu.main)
@@ -50,32 +53,34 @@ class MainActivity : AppCompatActivity() {
                     }
                     return@OnMenuItemClickListener true
                 }
-                R.id.action_refresh -> {
-                    load()
+                R.id.action_copy -> {
+                    if (mWhatTheCommitData != null) {
+                        // Gets a handle to the clipboard service.
+                        val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+                        // Creates a new text clip to put on the clipboard
+                        val clip = ClipData.newPlainText(mWhatTheCommitData!!.url, mWhatTheCommitData!!.message)
+                        clipboard.primaryClip = clip
+
+                        mRoot?.snack("Copied to clipboard");
+                    } else {
+                        mRoot?.snack("Nothing to copy yet");
+                    }
                     return@OnMenuItemClickListener true
                 }
             }
             false
         })
         mMessage?.setOnClickListener {
-            if (mWhatTheCommitData != null) {
-                // Gets a handle to the clipboard service.
-                val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-                // Creates a new text clip to put on the clipboard
-                val clip = ClipData.newPlainText(mWhatTheCommitData!!.url, mWhatTheCommitData!!.message)
-                clipboard.primaryClip = clip
-
-                mRoot?.snack("Copied to clipboard");
-            } else {
-                mRoot?.snack("Nothing to copy yet");
-            }
+            load()
         }
         load()
     }
 
     private fun load() {
+        showLoading()
         WhatTheCommitClient.instance().getMessage(WhatTheCommitClient.API_URL).enqueue(object : Callback<WhatTheCommitData> {
             override fun onResponse(call: Call<WhatTheCommitData>, response: Response<WhatTheCommitData>) {
+                hideLoading()
                 if (response.isSuccessful) {
                     mWhatTheCommitData = response.body()
                     mMessage?.text = mWhatTheCommitData!!.message
@@ -86,9 +91,20 @@ class MainActivity : AppCompatActivity() {
 
             override fun onFailure(call: Call<WhatTheCommitData>, t: Throwable) {
                 Timber.e(t, null)
+                hideLoading()
                 showError()
             }
         })
+    }
+
+    private fun showLoading() {
+        mMessage?.animate()?.alpha(0.0f);
+        mProgress?.animate()?.alpha(1.0f);
+    }
+
+    private fun hideLoading() {
+        mMessage?.animate()?.alpha(1.0f);
+        mProgress?.animate()?.alpha(0.0f);
     }
 
     private fun showError() {
---
+++ app/src/main/java/com/commit451/commitspiration/model/WhatTheCommitData.kt
@@ -0,0 +1,6 @@
+package com.commit451.commitspiration.model
+
+/**
+ * All the data for a WhatTheCommit message
+ */
+class WhatTheCommitData(val message: String, val url: String)
